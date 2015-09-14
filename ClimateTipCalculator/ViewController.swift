725c20a8a5ead6147ad83140bae15979a39c56e7
Michael Droz <michael.droz@gmail.com>
2015-09-13 22:42:59 -0700

    Initial Commit

---
+++ ClimateTipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClimateTipCalculator
+//
+//  Created by Michael Droz on 9/13/15.
+//  Copyright © 2015 Michael Droz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
